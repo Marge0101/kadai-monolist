03c7c420b6cc4e3aa803c53fce2208107d9f6430
Mari Sato <marge0101.ms@gmail.com>
2018-02-01 08:14:30 +0000

    design fix

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,14 @@ Rails.application.routes.draw do
   get 'login', to:'sessions#new'
   post 'login', to:'sessions#create'
   delete 'logout', to:'sessions#destroy'
+  get 'rankings/want', to: 'rankings#want'
+  get 'rankings/have', to: 'rankings#have'
   
   resources :users, only: [:show, :new, :create]
   #want 機能実装
   resources :items, only: [:new,:show]
   #resources :items, only: [:new]
   resources :ownerships, only: [:create, :destroy]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
